df4b2f435d22520615ec0d4f0815ff44bf991558
Titich_W <pierreconod@gmail.com>
2022-02-18 13:34:56 +0100

    Revert "ajout detail fichier v3"
    This reverts commit 98e1e66577c37da88254142c9f14eef9522effab.

---
+++ auto/cours_actuel.php
@@ -0,0 +1,59 @@
+<?php
+$api_exchangerate= null;
+include_once dirname(__FILE__) . '/../structure/sql.php';
+require dirname(__FILE__) . '/../vendor/autoload.php';
+require dirname(__FILE__) . '/../cle/cle.php';
+
+
+$req_url = 'https://v6.exchangerate-api.com/v6/'.$api_exchangerate.'/latest/CHF';
+$response_json = file_get_contents($req_url);
+
+// Continuing if we got a result
+if(false !== $response_json) {
+
+    // Try/catch for json_decode operation
+    try {
+
+        // Decoding
+        $response = json_decode($response_json);
+
+        // Check for success
+        if('success' === $response->result) {
+
+            // YOUR APPLICATION CODE HERE, e.g.
+            //$base_price = 12; // Your price in USD
+            //$valeur =  round(($valeur * $response->conversion_rates->CHF), 2);
+            //$valeur2 =$response->conversion_rates->CHF;
+            $euro =$response->conversion_rates->EUR;
+            $dollar =$response->conversion_rates->USD;
+
+        }
+
+    }
+    catch(Exception $e) {
+        // Handle JSON parse error...
+    }
+
+}
+
+/*var_dump($valeur);
+var_dump("<hr>");
+var_dump($valeur2);
+var_dump("<hr>");*/
+
+
+
+
+$sql_cours = "INSERT INTO `cours_acuel` (`cours_acuel_id`, `cours_acuel_euro`, `cours_acuel_dollar`, `cours_acuel_date`)\n"
+    . "VALUES (NULL, '".$euro."', '".$dollar."', NOW());";
+
+//echo "<p>".$sql_cours."</p>";
+mysqli_query ($ezine_db, $sql_cours) or ezine_mysql_die($ezine_db, $sql_cours) ;
+
+
+
+
+
+
+?>
+
